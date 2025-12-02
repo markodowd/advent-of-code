93a2cc302cf921060012f4a79991d9317d8ec3bd
Mark O'Dowd <contact@markodowd.dev>
2025-12-02 10:37:30 +0000

    refactor: function not needed

--- 2025/1/1a.py
+++ 2025/1/1a.py
@@ -1,31 +1,28 @@
-def count_instructions():
-    count = 0
-    position = 50
+count = 0
+position = 50
 
-    with open('./input.txt', 'r') as file:
-        for line in file:
-            instruction = line.strip()
-            direction = instruction[0]
-            rotation = int(instruction[1:])
+with open("./input.txt", "r") as file:
+    for line in file:
+        instruction = line.strip()
+        direction = instruction[0]
+        rotation = int(instruction[1:])
 
-            while rotation > 0:
-                if direction == 'L':
-                    position -= 1
-                    rotation -= 1
+        while rotation > 0:
+            if direction == "L":
+                position -= 1
+                rotation -= 1
 
-                if direction == 'R':
-                    position += 1
-                    rotation -= 1
+            if direction == "R":
+                position += 1
+                rotation -= 1
 
-                if position == 100:
-                    position = 0
+            if position == 100:
+                position = 0
 
-                if position == -1:
-                    position = 99
+            if position == -1:
+                position = 99
 
-            if position == 0:
-                count += 1
-    
-    print(count) 
+        if position == 0:
+            count += 1
 
-count_instructions()
+print(count)
